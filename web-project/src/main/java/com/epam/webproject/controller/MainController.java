6644d863f69f9e06f892aad02a95f7bdc7d4debb
Vladislav Lisetskii <vladlis54@gmail.com>
2017-05-01 19:25:41 +0300

    Minor fixes

--- web-project/src/main/java/com/epam/webproject/controller/MainController.java
+++ web-project/src/main/java/com/epam/webproject/controller/MainController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/")
 public class MainController {
+
     @RequestMapping(method = RequestMethod.GET)
     public String printWelcome(ModelMap model) {
         model.addAttribute("message", "Hello, %username%!");
