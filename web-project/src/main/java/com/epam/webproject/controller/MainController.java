83ea1af09901fe6db6214c651bf151a3428f1f65
Vladislav Lisetskii <vladislav_lisetskii@epam.com>
2017-05-02 20:32:00 +0300

    Add Spring Security

--- web-project/src/main/java/com/epam/webproject/controller/MainController.java
+++ web-project/src/main/java/com/epam/webproject/controller/MainController.java
@@ -1,9 +1,15 @@
 package com.epam.webproject.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 @RequestMapping("/")
@@ -13,5 +19,29 @@ public class MainController {
     public String printWelcome(ModelMap model) {
         model.addAttribute("message", "Hello, %username%!");
         return "index";
+    }
+
+    @RequestMapping("user")
+    public String getUserPage() {
+        return "user/home";
+    }
+
+    @RequestMapping("admin")
+    public String getAdminPage() {
+        return "admin/home";
+    }
+
+    @RequestMapping("logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/";
+    }
+
+    @RequestMapping("accessDenied")
+    public String accessDenied() {
+        return "accessDenied";
     }
 }
---
+++ web-project/src/main/webapp/WEB-INF/pages/admin/home.jsp
@@ -0,0 +1,12 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+
+<html>
+<head>
+    <title>Admin Home</title>
+</head>
+<body>
+This is a home page for admin.
+<br>
+<a href="/logout">Logout</a>
+</body>
+</html>
